efc6bdbb6c0a6b82cf630c8cca407fa02f08d293
ahnoldmoon <munnt72@hotmail.com>
2019-03-17 10:49:22 +0900

    Updates

---
+++ precache-manifest.8a70d1104b056ec7daa00a49ce17071b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3d07953dfb554cfb69",
+    "url": "/movie-app/static/js/runtime~main.6e3d0795.js"
+  },
+  {
+    "revision": "f39e8cf54ced7d70b7f8",
+    "url": "/movie-app/static/js/main.f39e8cf5.chunk.js"
+  },
+  {
+    "revision": "24dde547527b2142620d",
+    "url": "/movie-app/static/js/2.24dde547.chunk.js"
+  },
+  {
+    "revision": "f39e8cf54ced7d70b7f8",
+    "url": "/movie-app/static/css/main.3c52b8af.chunk.css"
+  },
+  {
+    "revision": "c42ef2ffb0db67c8da93ab32ca767118",
+    "url": "/movie-app/index.html"
+  }
+];
